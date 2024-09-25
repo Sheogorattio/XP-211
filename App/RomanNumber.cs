4b507aa4081305a95f347523e1775b086e46d273
Sheogorattio <91422100+Sheogorattio@users.noreply.github.com>
2024-09-25 07:15:46 +0300

    new test cases (test cases 7 )

--- App/RomanNumber.cs
+++ App/RomanNumber.cs
@@ -14,36 +14,70 @@ namespace App
         // public int Value { get; } = Value;
         public static RomanNumber Parse(string input)
         {
+            CheckSymbols(input);
+
+
+            String argValue = input;
+            input = input.Trim();
+
             int prevValue = 0;
             int result = 0;
             int position = 0;
-            foreach(char c in input)
+            foreach (char c in input)
             {
-                try
+                int currentValue = DigitalValue(c);
+                //validate number or thorw excption
+
+                if (currentValue > prevValue)
                 {
-                   int currentValue = DigitalValue(c);
-                    if (currentValue >prevValue)
-                    {
-                        result += currentValue - 2 * prevValue;
-                    }
-                    else
-                    {
-                        result += currentValue;
-                    }
-                    prevValue = currentValue;
+                    result += currentValue - 2 * prevValue;
                 }
-                catch
+                else
                 {
-                    throw new FormatException($"RomanNumber.Parse: invalid input {input} has illegal char '{c}' at position {position}");
+                    result += currentValue;
                 }
+                if (prevValue != 0 && prevValue < currentValue)
+                {
+                    //проверка на правильность последовательности символов в парах (например, как в II(правильно) и в IL(не правильно))
+                    if (currentValue / prevValue > 10 || prevValue.ToString()[0] == '5')
+                    {
+                        throw new FormatException($"RomanNumber.Parse('{argValue}') error: '{input[position - 1]} before {c}' in position position {position - 1}");
+                    }
+                }
+
+                if (currentValue == 0 && input.Length > 0)
+                {
+                    throw new FormatException($"RomanNumber.Parse('{argValue}') error: 'digit 'N' must not be in number' in position {position}");
+                }
+
+                //!--------------------------------------------------------------------------------------------------------
+
+                List<char> repetableSymbols = new List<char>() { 'I', 'X', 'C', 'M' };
+                List<char> unrepeatableSymbols = new List<char>() { 'V', 'L', 'D' };
+                int lessCount = 0;
+
+                for (int i = 0; i < position; i++)
+                {
+                    if (DigitalValue(input[i]) < currentValue)
+                    {
+                        lessCount++;
+                    }
+                    else break;
+                    if (lessCount > 1)
+                    {
+                        throw new FormatException($"RomanNumber.Parse({input}) error: more than one less digit before {input[position]} at position {position}. Invalid input {input}");
+                    }
+                }
+
+                prevValue = currentValue;
                 position++;
             }
-           return new RomanNumber(result);
+            return new RomanNumber(result);
         }
 
         public static int DigitalValue(char digit) => digit switch
         {
-            'N'=> 0,
+            'N' => 0,
             'I' => 1,
             'V' => 5,
             'X' => 10,
@@ -51,7 +85,24 @@ namespace App
             'C' => 100,
             'D' => 500,
             'M' => 1000,
-            _ => throw new ArgumentException($"'RomanNumber.DigitalValue': argument 'digit' has invalid value '{digit}'") { Source= "RomanNumber.DigitalValue" }
+            _ => throw new ArgumentException($"'RomanNumber.DigitalValue': argument 'digit' has invalid value '{digit}'") { Source = "RomanNumber.DigitalValue" }
         };
+
+        public static void CheckSymbols(String input)
+        {
+            String argValue = input;
+            input = input.Trim();
+            for (int i = 0; i < input.Length; i++)
+            {
+                try
+                {
+                    DigitalValue(input[i]);
+                }
+                catch
+                {
+                    throw new FormatException($"RomanNumber.Parse: invalid input '{argValue}' has illegal char '{input[i]}' at position {i}");
+                }
+            }
+        }
     }
 }
--- Tests/RomanNumberTest.cs
+++ Tests/RomanNumberTest.cs
@@ -1,5 +1,7 @@
 using App;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 using System;
+using System.ComponentModel;
 
 namespace Tests
 {
@@ -9,26 +11,11 @@ namespace Tests
         [TestMethod]
         public void ParseTest()
         {
-            //Dictionary<String, int> testCases = new()
-            //{
-            //   { "I", 1 },
-            //   { "II", 2 },
-            //   { "III", 3 },
-            //    //{ "N", 0},
-            //    //{ "V", 5},
-            //    //{ "X", 10},
-            //    //{ "L", 50},
-            //    //{ "C", 100},
-            //    //{ "M", 1000 },
-            //};
-            //foreach (var _testCase in testCases)
-            //{
-            //    Assert.AreEqual(_testCase.Value, RomanNumber.Parse(_testCase.Key).Value, $"Roman value {_testCase.Value} -> {_testCase.Key}");
-            //}
-
+            const int maxValue = 4000;
             Dictionary<String, int> validCases = new()
             {
-                {"IV", 4},    
+                {"IV", 4},
+                {"IIII", 4},
                 {"IX", 9},      
                 {"XL", 40},     
                 {"XC", 90},     
@@ -37,8 +24,10 @@ namespace Tests
                 {"XVI", 16},    
                 {"LXV", 65},   
                 {"XCIX", 99},  
-                {"DCCC", 800},  
-                {"MMXXIV", 2024} 
+                {"DCCC", 800},
+                {"MMMCMXCIX", 3999 },
+                {"MMXXIV", 2024} ,
+                {"MMMM", 4000},
             };
             foreach (var validCase in validCases)
             {
@@ -59,9 +48,101 @@ namespace Tests
                 var ex = Assert.ThrowsException<FormatException>(() => { RomanNumber.Parse(testCase[0]); }, $"RomanNumber.Parse('{testCase[0]}') must throw FormatException");
                 Assert.IsTrue(ex.Message.Contains("RomanNumber.Parse"), $"ex.Message must contain origin (class and method):" + $"testCase='{testCase[0]}', ex.message='{ex.Message}'");
                 Assert.IsTrue(ex.Message.Contains($"{testCase[0]}"), $"ex.Message must contain input value '{testCase[0]}':" + $"testCase='{testCase[0]}', ex.message='{ex.Message}'");
-                Assert.IsTrue(ex.Message.Contains($"{testCase[1]}") && ex.Message.Contains($"position {testCase[2]}"), $"ex.Message must contain error char '{testCase[1]}' and its position {testCase[2]}: " + $"testCase='{testCase[0]}', ex.message='{ex.Message}'");
+                Assert.IsTrue(ex.Message.Contains($"{testCase[1]}") && ex.Message.Contains($"position {testCase[2]}"),
+                    $"ex.Message must contain error char '{testCase[1]}' and its position {testCase[2]}: " + $"testCase='{testCase[0]}', ex.message='{ex.Message}'");
             }
 
+            String[][] testCases2 = //парні символи(недозволені)
+            [
+                ["IL", "I", "L", "0"],
+                ["IC", "I", "C", "0"],
+                ["ID", "I", "D", "0"],
+                ["IM", "I", "M", "0"],
+                ["XM", "X", "M", "0"],
+                ["XD", "X", "D", "0"],
+                ["VX", "V", "X", "0"],
+                ["VM", "V", "M", "0"],
+
+            ];
+            foreach (var testCase in testCases2)
+            {
+                var ex = Assert.ThrowsException<FormatException>(() => { RomanNumber.Parse(testCase[0]); }, $"RomanNumber.Parse('{testCase[0]}') must throw FormatException");
+                Assert.IsTrue(ex.Message.Contains("RomanNumber.Parse"), $"ex.Message must contain origin (class and method):" + $"testCase='{testCase[0]}', ex.message='{ex.Message}'");
+                Assert.IsTrue(ex.Message.Contains($"{testCase[0]}"), $"ex.Message must contain input value '{testCase[0]}':" + $"testCase='{testCase[0]}', ex.message='{ex.Message}'");
+                Assert.IsTrue(ex.Message.Contains($"{testCase[1]} before {testCase[2]}") && ex.Message.Contains($"position {testCase[3]}"),
+                    $"ex.Message must contain <'{testCase[1]}'> before <'{testCase[2]}> and error position {testCase[3]}: " + $"testCase='{testCase[0]}', ex.message='{ex.Message}'");
+            }
+
+            String[][] testCases3 = //неприпустимість цифри  0  у числах
+            [
+                ["VN","1"],
+                ["NV","0"]
+            ];
+            foreach (var testCase in testCases3)
+            {
+                var ex = Assert.ThrowsException<FormatException>(() => { RomanNumber.Parse(testCase[0]); }, $"RomanNumber.Parse('{testCase[0]}') must throw FormatException");
+                Assert.IsTrue(ex.Message.Contains("RomanNumber.Parse"), $"ex.Message must contain origin (class and method):" + $"testCase='{testCase[0]}', ex.message='{ex.Message}'");
+                Assert.IsTrue(ex.Message.Contains($"{testCase[0]}"), $"ex.Message must contain input value '{testCase[0]}':" + $"testCase='{testCase[0]}', ex.message='{ex.Message}'");
+                Assert.IsTrue(ex.Message.Contains($"digit 'N' must not be in number") && ex.Message.Contains($"position {testCase[1]}"),
+                    $"ex.Message must contain <digit 'N' must not be in number> and error position {testCase[1]}: " + $"testCase='{testCase[0]}', ex.message='{ex.Message}'");
+            }
+
+
+            String[][] testCases4 = //неприпустимість пробілів (не дозволяємо всередині числа)
+           [
+                ["V I","1"],
+                ["X\tX","1"],
+                ["X\nX","1"],
+                ["X\0X","1"],
+           ];
+            foreach (var testCase in testCases4)
+            {
+                var ex = Assert.ThrowsException<FormatException>(() => { RomanNumber.Parse(testCase[0]); }, $"RomanNumber.Parse('{testCase[0]}') must throw FormatException");
+              
+            }
+
+
+            String[] testCases5 = //неприпустимість пробілів (не дозволяємо всередині числа)
+           [
+                "XX", "CC", "  CC", "    XX", "\tIV", "XI\t\t", "CD\n"
+           ];
+            foreach (var testCase in testCases5)
+            {
+                Assert.IsNotNull(RomanNumber.Parse(testCase),
+                    "Ceiling and trailing spaces are allowed");
+            }
+
+            String[][] testCases6 = //контроль позицій з пробілами
+           [
+                ["    XSV", "S","1"],
+                [" VC ","V", "0"],
+           ];
+            foreach (var testCase in testCases6)
+            {
+                var ex = Assert.ThrowsException<FormatException>(() => { RomanNumber.Parse(testCase[0]); }, $"RomanNumber.Parse('{testCase[0]}') must throw FormatException");
+                Assert.IsTrue(ex.Message.Contains($"{testCase[1]}") && ex.Message.Contains($"position {testCase[2]}"),
+                                    $"ex.Message must contain error char '{testCase[1]}' and its position {testCase[2]}: " + $"testCase='{testCase[0]}', ex.message='{ex.Message}'");
+                Assert.IsTrue(ex.Message.Contains("RomanNumber.Parse"), $"ex.Message must contain origin (class and method):" + $"testCase='{testCase[0]}', ex.message='{ex.Message}'");
+                Assert.IsTrue(ex.Message.Contains($"{testCase[0]}"), $"ex.Message must contain input value '{testCase[0]}':" + $"testCase='{testCase[0]}', ex.message='{ex.Message}'");
+            }
+
+            String[][] testCases7 = //декілька менших цифр ліворуч від більшої
+            [
+                ["IIX", "X","2"], // при цьому кожна ПАРА цифр  - нормальна
+                ["IVIX","X", "3"],
+                ["IXCM", "C","2"],
+                ["XLXC", "C","3"],
+                ["IIV", "V","2"],
+            ];
+            foreach (var testCase in testCases7)
+            {
+                var ex = Assert.ThrowsException<FormatException>(() => { RomanNumber.Parse(testCase[0]); }, $"RomanNumber.Parse('{testCase[0]}') must throw FormatException");
+                Assert.IsTrue(ex.Message.Contains($"{testCase[1]}") && ex.Message.Contains($"position {testCase[2]}"),
+                                    $"ex.Message must contain error char '{testCase[1]}' and its position {testCase[2]}: " + $"testCase='{testCase[0]}', ex.message='{ex.Message}'");
+                Assert.IsTrue(ex.Message.Contains("RomanNumber.Parse"), $"ex.Message must contain origin (class and method):" + $"testCase='{testCase[0]}', ex.message='{ex.Message}'");
+                Assert.IsTrue(ex.Message.Contains($"{testCase[0]}"), $"ex.Message must contain input value '{testCase[0]}':" + $"testCase='{testCase[0]}', ex.message='{ex.Message}'");
+                Assert.IsTrue(ex.Message.Contains($"more than one less digit before {testCase[1]}"), $"ex.Message must contain input valeue 'more than one less digit before {testCase[1]}':" + $"testCase='{testCase[0]}', ex.message='{ex.Message}'");
+            }
         }
 
         [TestMethod]
